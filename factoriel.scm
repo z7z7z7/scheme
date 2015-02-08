92f0c3b8c22a0c91caf67a7674a27a38a0b3f3ea
z7z7z7 <natalia.astadjova@gmail.com>
2015-02-08 16:01:36 +0200

    Update factoriel.scm

--- factoriel.scm
+++ factoriel.scm
@@ -2,3 +2,10 @@
   (if (<= n 1)
       1
       (* n (fact (- n 1)))))
+      
+(define (fact-iter n)
+  (define (helper i acc)
+    (if (= i 1)
+        acc
+        (helper (- i 1) (* acc i))))
+  (helper n 1))
